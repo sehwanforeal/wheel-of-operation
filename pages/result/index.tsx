18d5a52aec81bae38a55de9e108079f36e92fe9f
sehwanforeal <55143030+sehwanforeal@users.noreply.github.com>
2021-04-08 19:26:22 +0900

    fix: 필요없는 import를 제거한다

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import Button from "../../components/uiParts/Button";
-import { useRouter } from "next/router";
 
 export default function Result() {
   return <Container>결과</Container>;
